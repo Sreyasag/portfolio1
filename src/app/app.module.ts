dc861937badea456c3c6eb597d4f5df3c3ce388f
Sreyas <64139020+Sreyasag@users.noreply.github.com>
2022-07-13 20:49:09 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LandingComponent } from './landing/landing.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LandingComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
