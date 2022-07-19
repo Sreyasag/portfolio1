6f0f01d0a81f3c5be91bdbc46763eeb45b420d98
Sreyas <64139020+Sreyasag@users.noreply.github.com>
2022-07-19 11:10:36 +0530

    ngfor project

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -3,13 +3,42 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css']
+  styleUrls: ['./project.component.css'],
 })
 export class ProjectComponent implements OnInit {
+  projects = [
+    {
+      title: 'Kerala Tourism',
+      image: 'f2.jpg',
+      link: 'https://sreyasag.github.io/Kerala/',
+      details:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat vero tenetur tempore itaque totam eum veniam, accusantium cumque ullam voluptas. Laboriosam, et reiciendis voluptatibus aut tempore magni animi dolores at.',
+    },
+    {
+      title: 'Todo App',
+      image: 'f2.jpg',
+      link: 'https://sreyasag.github.io/todo/todo.html',
+      details:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat vero tenetur tempore itaque totam eum veniam, accusantium cumque ullam voluptas. Laboriosam, et reiciendis voluptatibus aut tempore magni animi dolores at.',
+    },
+    {
+      title: 'Project title',
+      image: 'f2.jpg',
+      link: ' ',
+      details:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat vero tenetur tempore itaque totam eum veniam, accusantium cumque ullam voluptas. Laboriosam, et reiciendis voluptatibus aut tempore magni animi dolores at.',
+    },
+    {
+      title: 'Project title',
+      image: 'f2.jpg',
+      link: ' ',
+      details:
+        'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat vero tenetur tempore itaque totam eum veniam, accusantium cumque ullam voluptas. Laboriosam, et reiciendis voluptatibus aut tempore magni animi dolores at.',
+    },
+    
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
